63d8c75808c3fe4aa3121bbf8b3e9787ef913629
Dovlane <vlada.ignjatije@gmail.com>
2024-06-01 19:31:33 +0200

    Base for _add_ and _stop_ command is implemented

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int
 main()
 {
     printf("izvrsio se main\n"); 
+    while (1) {
+        char* command;
+        scanf("%s", command);
+        if (strcmp(command, "_add_") == 0) {
+            printf("I recognised add\n");
+        }
+        else if (strcmp(command, "_stop_") == 0) {
+            printf("_stop_ -> main thread is destroyed\n");
+            break;
+        }
+    }
     return 0;
 }
