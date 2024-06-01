86214652efdb6c68376f00834119555c65d01269
Dovlane <vlada.ignjatije@gmail.com>
2024-06-01 20:22:07 +0200

    reading file paths implemented

--- Makefile
+++ Makefile
@@ -1,5 +1,25 @@
-all:
-	gcc main.c -o main -pthread
+TARGET = prog
+LIBS = -lm -pthread
+CC = gcc
+CFLAGS= -g -Wall -Wextra
+
+.PHONY: run all clean
+
+run: $(TARGET)
+	./prog
+all: run
+
+OBJECTS = $(patsubst %.c, %.o, $(wildcard *.c))
+HEADERS = $(wildcard *.h)
+
+%.o: %.c $(HEADERS)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+.PRECIOUS: $(TARGET) $(OBJECTS)
+
+$(TARGET): $(OBJECTS)
+	$(CC) $(OBJECTS) -Wall $(LIBS) -o $@
 
 clean:
-	gcc main
+	-rm -f *.o
+	-rm -f $(TARGET)
--- main.c
+++ main.c
@@ -6,16 +6,30 @@ int
 main()
 {
     printf("izvrsio se main\n"); 
+    char** directories;
+    int size = 1;
+    directories = malloc(sizeof(char* ));
     while (1) {
-        char* command;
+        char command[10];
         scanf("%s", command);
         if (strcmp(command, "_add_") == 0) {
-            printf("I recognised add\n");
+            char directory[50];
+            scanf("%s", directory);
+            //printf("sizeof(%s) = %d\n", directory, (int)sizeof(directory));
+
+            directories[size - 1] = calloc(sizeof(directory) + 1, sizeof(char));
+            memmove(directories[size - 1], directory, sizeof(directory));
+            directories = realloc(directories, (++size) * sizeof(char* ));
         }
         else if (strcmp(command, "_stop_") == 0) {
             printf("_stop_ -> main thread is destroyed\n");
             break;
         }
     }
+    for (int i = 0; i < size; i++) {
+        //printf("directories[%d] = %s\n", i,  directories[i]);
+        free(directories[i]);
+    }
+    free(directories);
     return 0;
 }
